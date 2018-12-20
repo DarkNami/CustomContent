450940147cdf292de78cabbcb5154b7ea0db898e
Dragon Chuang <dragon@fandratec.com>
2018-12-20 19:00:58 +0800

    style: Reformat code
    * fix indents (convert Space to Tab)
    * Change array() to 'short array syntax'

--- CustomContent.php
+++ CustomContent.php
@@ -11,54 +11,51 @@
 
 class CustomContentPlugin extends MantisPlugin {
 
-    public function register() {
-        $this->name = 'CustomContent Plugin';
-        $this->description = 'Enables the usage of various configuration parameters to insert custom HTML, PHP, JS and/or CSS content in the Mantis HTML layout';
+	public function register() {
+		$this->name = 'CustomContent Plugin';
+		$this->description = 'Enables the usage of various configuration parameters to insert custom HTML, PHP, JS and/or CSS content in the Mantis HTML layout';
+		$this->version = '0.1.0';
+		$this->requires = ['MantisCore' => '2.0.0'];
+		$this->author = 'Ryadel';
+		$this->contact = 'web@ryadel.com';
+		$this->url = 'https://www.ryadel.com/';
+	}
 
-        $this->version = '0.1.0';
-        $this->requires = array(
-        'MantisCore' => '2.0.0'
-        );
-
-        $this->author = 'Ryadel';
-        $this->contact = 'web@ryadel.com';
-        $this->url = 'https://www.ryadel.com/';
-    }
-
-    public function hooks() {
-        return array(
-            'EVENT_LAYOUT_RESOURCES' => 'custom_head',
-            'EVENT_LAYOUT_BODY_BEGIN' => 'custom_body_begin',
-            'EVENT_LAYOUT_BODY_END' => 'custom_body_end',
+	public function hooks() {
+		return [
+			'EVENT_LAYOUT_RESOURCES' => 'custom_head',
+			'EVENT_LAYOUT_BODY_BEGIN' => 'custom_body_begin',
+			'EVENT_LAYOUT_BODY_END' => 'custom_body_end',
 			'EVENT_LAYOUT_PAGE_HEADER' => 'custom_page_header',
 			'EVENT_LAYOUT_PAGE_FOOTER' => 'custom_page_footer'
-        );
-    }
+		];
+	}
 
-    public function custom_head($p_event) {
+	public function custom_head($p_event) {
 		$this->custom_render('custom_head_file');
-    }
+	}
 
 	public function custom_body_begin($p_event) {
 		$this->custom_render('custom_body_begin_file');
-    }
+	}
 
 	public function custom_body_end($p_event) {
 		$this->custom_render('custom_body_end_file');
-    }
-	
+	}
+
 	public function custom_page_header($p_event) {
 		$this->custom_render('custom_page_header_file');
-    }
-	
+	}
+
 	public function custom_page_footer($p_event) {
 		$this->custom_render('custom_page_footer_file');
-    }
-	
+	}
+
 	private function custom_render($key) {
 		$t_page = config_get_global($key);
-		if( !is_blank( $t_page ) && file_exists( $t_page ) && !is_dir( $t_page ) ) {
-			include( $t_page );
+		if (!is_blank($t_page) && file_exists($t_page) && !is_dir($t_page)) {
+			include $t_page;
 		}
 	}
+
 }
